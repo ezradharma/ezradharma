25e3a6597de3c4ca46f9087e806f4a26cb80aedd
ezradharma <ezradharma@gmail.com>
2021-08-04 19:33:46 -0400

    In-Class 5 Addon

---
+++ IA5/js/example.js
@@ -0,0 +1,41 @@
+/*
+File: example.js
+GUI Assignment: inClass 5
+Ezra Dharma, UMass Lowell Computer Science, edharma@cs.uml.edu
+Copyright (c) 2021 by Ezra. All rights reserved. May be freely copied or excerpted for educational purposes with credit to the author.
+updated by ED on Aug 4, 2021 at 7:30 PM
+js to edit groceries list.
+*/
+
+var uList = document.getElementById("page").getElementsByTagName("ul") [0];
+
+// ADD NEW ITEM TO END OF LIST
+
+var newItem1 = document.createElement("li"), newtxt1 = document.createTextNode("cream");
+newItem1.appendChild(newtxt1);
+uList.appendChild(newItem1);
+
+// ADD NEW ITEM START OF LIST
+
+var newItem2 = document.createElement("li"), newtxt2 = document.createTextNode("kale");
+newItem2.appendChild(newtxt2);
+uList.prepend(newItem2);
+
+// ADD A CLASS OF COOL TO ALL LIST ITEMS
+
+var allItems = document.querySelectorAll('li');
+allItems.forEach((allItems) => (allItems.className += "COOL"));
+
+
+// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+
+var header = document.getElementById("page").getElementsByTagName('h2') [0];
+
+header.innerHTML += '<span style="color:white">' + allItems.length + '</span>'; //<span> colors text
+
+//Change list bg color
+
+//color #6CC0AC, top #76D3BD, bot #509080
+allItems.forEach((allItems) => (allItems.style.backgroundColor = "#6CC0AC"));
+allItems.forEach((allItems) => (allItems.style.borderTopColor = "#76D3BD"));
+allItems.forEach((allItems) => (allItems.style.borderBottomColor = "#509080"));
